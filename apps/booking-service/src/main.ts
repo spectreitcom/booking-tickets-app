1f837a214fe29698749c85ae39688f6777188974
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2026-08-04 13:01:45 +0200

    chore: fix errors and warnings

--- apps/booking-service/src/main.ts
+++ apps/booking-service/src/main.ts
@@ -5,4 +5,4 @@ async function bootstrap() {
   const app = await NestFactory.create(BookingServiceModule);
   await app.listen(process.env.port ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) => console.error(err));
--- apps/gateway-service/test/app.e2e-spec.ts
+++ apps/gateway-service/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { GatewayServiceModule } from './../src/gateway-service.module';
+import request from 'supertest';
+import { GatewayServiceModule } from '../src/gateway-service.module';
 
 describe('GatewayServiceController (e2e)', () => {
   let app: INestApplication;
@@ -16,7 +16,7 @@ describe('GatewayServiceController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return request(app.getHttpServer() as import('http').Server)
       .get('/')
       .expect(200)
       .expect('Hello World!');
